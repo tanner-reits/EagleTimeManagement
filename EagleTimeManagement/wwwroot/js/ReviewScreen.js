dbf21eefb3c68a4453880ac167678fa22ce07ff1
Tanner <tannerreits@gmail.com>
2019-06-01 18:06:40 -0400

    feaet(reviewScreen): Added dynamic label loading

---
+++ EagleTimeManagement/wwwroot/js/ReviewScreen.js
@@ -0,0 +1,13 @@
+﻿var ddl = document.querySelector('#DateRange');
+
+ddl.addEventListener('change', function () {
+    var span    = document.querySelector('#display-ddl')
+    var ddlText = ddl.options[ddl.selectedIndex].text;
+
+    if (ddlText === 'Select Date Range') {
+        span.innerHTML = '';
+    }
+    else {
+        span.innerHTML = ddlText;
+    }
+})
